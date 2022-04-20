e46bed00cb127e96a903faae92e3dad3e94d1293
marc_ <marc_@DESKTOP-30L4RPF>
2022-04-19 21:51:00 -0400

    ko

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -55,7 +55,6 @@ export default class Game extends Phaser.Scene {
     }
 
     update() {
-        scene.GameHandler.refreshCards
         this.playerCardZoneText?.setText(this.socket.id);
     }
 }
